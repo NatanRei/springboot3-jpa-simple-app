1bb11d209c9903fcc8d64d032d2b1b7d0f5fe998
NatanRei <nreischmura@gmail.com>
2023-07-18 13:15:29 -0300

    feat: Order, Instant, ISO 8601

---
+++ src/main/java/com/ntnconsultoria/project/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.ntnconsultoria.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ntnconsultoria.project.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+	
+}
