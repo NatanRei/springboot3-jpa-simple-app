c853683bb79c3bb7a356f45f81dd0c0b78d01976
NatanRei <nreischmura@gmail.com>
2023-07-18 17:42:47 -0300

    feat: Exception handing delete

--- src/main/java/com/ntnconsultoria/project/services/UserService.java
+++ src/main/java/com/ntnconsultoria/project/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.ntnconsultoria.project.entities.User;
 import com.ntnconsultoria.project.repositories.UserRepository;
+import com.ntnconsultoria.project.services.exceptions.DatabaseException;
 import com.ntnconsultoria.project.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,13 +33,24 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);		
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
