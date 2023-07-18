be7a729739aba72e4c38f7eef622f9dbe5dd1b4a
NatanRei <nreischmura@gmail.com>
2023-07-18 12:50:07 -0300

    feat: Service layer, component registration

--- src/main/java/com/ntnconsultoria/project/resources/UserResource.java
+++ src/main/java/com/ntnconsultoria/project/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.ntnconsultoria.project.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ntnconsultoria.project.entities.User;
+import com.ntnconsultoria.project.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Natan", "natan@teste.com", "12345678", "123");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> users = service.findAll();
+		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 	
 }
